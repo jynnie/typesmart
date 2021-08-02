a494920f597314687280a4b478fa49e21a8b8f14
jynnie <jynnie@mit.edu>
2021-08-02 12:58:36 -0700

    ⭐️ Basic app setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import "antd/dist/antd.dark.css";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
